0e684ca57e726c068aad879edc0ee46f6e1c3423
Tom Guerrand <tomguerrand7@gmail.com>
2022-06-14 16:08:50 +0200

    last commit

--- BatteryProject/data.py
+++ BatteryProject/data.py
@@ -4,13 +4,19 @@ from BatteryProject.params import *
 
 def get_data():
     print("Entrez le nom d'une feature(fichier)")
-    FEATURE_NAME = f'transformed_data/{input()}.csv'
-    a = input("Entrez le premier index de la colonne souhaité")
-    b = input("Entrez le dernier index de la colonne souhaité")
+
+    FEATURE_NAME = f'{input()}.csv'
+    #FEATURE_NAME = "Temporary_data/summary-charge-duration.csv"
+    a = int(input("Entrez le premier index de la colonne souhaité : "))
+    b = int(input("Entrez le dernier index de la colonne souhaité : "))
     NCOL = range(a, b)
-    df = pd.read_csv(f"gs://{BUCKET_NAME}/{FEATURE_NAME}", usecols=NCOL)
+
+
+    df = pd.read_csv(f"gs://{BUCKET_NAME}/Temporary_data/{FEATURE_NAME}", usecols=NCOL)
+
     return df
 
 
 if __name__ == '__main__':
     df = get_data()
+    df.info()
