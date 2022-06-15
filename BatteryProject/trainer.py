58f741ea7fe7706cc9c0234e8a29f7c49f74f707
Tom Guerrand <tomguerrand7@gmail.com>
2022-06-15 15:00:26 +0200

    lastpush

--- BatteryProject/trainer.py
+++ BatteryProject/trainer.py
@@ -3,25 +3,33 @@ import numpy as np
 import pandas as pd
 
 
+class Trainer(object):
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+        # for MLFlow
+        #self.experiment_name = EXPERIMENT_NAME
 
-def compute_distance(df):
-    """ function used in order to compute a distance feature for a dataframe """
-    pass
+    def preprocess(self, df):
+        """ function that pre-processes the data """
+        pass
+
+    def set_pipeline(self):
+        pass
+
+    def train_model(X_train, y_train):
+        """ function that trains the model """
+        pass
 
 
-def preprocess(df):
-    """ function that pre-processes the data """
-    pass
-
-
-def train_model(X_train, y_train):
-    """ function that trains the model """
-    pass
-
-
-def save_model(reg):
-    """ method that saves the model into a .joblib file and uploads it on Google Storage /models folder """
-    pass
+    def save_model(reg):
+        """ method that saves the model into a .joblib file and uploads it on Google Storage /models folder """
+        pass
 
 
 if __name__ == '__main__':
